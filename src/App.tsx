d5ec67bd91258fc00847dc52550e5c87c7c19b1b
Klint <klintwales@gmail.com>
2024-11-20 21:24:30 -0600

    start some styling changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,34 @@
 import './App.css'
 import {
+    Box,
     DrawerBackdrop, DrawerBody, DrawerFooter,
     DrawerHeader,
     DrawerRoot,
-    DrawerTitle, ProgressCircleRoot, Text
+    DrawerTitle, Grid, List, ProgressCircleRoot,
 } from "@chakra-ui/react";
 import {DrawerContent} from "@/components/ui/drawer.tsx";
 import {ProgressCircleRing} from "@/components/ui/progress-circle.tsx";
-
+import { Avatar } from "@/components/ui/avatar"
+import {useState} from "react";
 function App() {
-  return (
+    const [drawerOpen, setDrawerOpen] = useState(false)
+    return (
       <div>
-          <Text>Coming Soon!</Text>
+          <Grid backgroundColor={"aliceblue"} padding={4}>
+              <Box display="flex" justifyContent="right">
+                <Avatar variant="solid" name="Klint Wales" onClick={() => setDrawerOpen(!drawerOpen)}/>
+              </Box>
+          </Grid>
+          <List.Root>
+              <List.Item>Buy paper towels</List.Item>
+              <List.Item>Paint door</List.Item>
+          </List.Root>
+          
           <ProgressCircleRoot value={null}>
               <ProgressCircleRing />
           </ProgressCircleRoot>
-          <DrawerRoot>
+          
+          <DrawerRoot open={drawerOpen} onOpenChange={(e) => setDrawerOpen(e.open)}>
               <DrawerBackdrop />
               <DrawerContent>
                   <DrawerHeader>
