f3c9935a63d45b148e0a4f322dac1f5e9b3ea834
Klint <klintwales@gmail.com>
2024-12-21 22:02:11 -0600

    add reminder model

---
+++ src/models/reminder.tsx
@@ -0,0 +1,8 @@
+class Reminder {
+    private id: string;
+    private title: string;
+    private description: string;
+    private createdDate: Date;
+    private modifiedDate: Date;
+    private complete: boolean;
+}
